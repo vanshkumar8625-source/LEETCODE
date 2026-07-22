42903170c00ea74aae310f7e0dcca4a7b5e0ac1e
vanshkumar8625-source <vanshkumar8625@gmail.com>
2026-07-22 10:42:49 +0530

    Time: 0 ms (100%), Space: 8.1 MB (44.65%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.size() != goal.size()) {
+            return false;
+        }
+        string str = s + s;
+        if (str.find(goal) == string::npos) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+};
